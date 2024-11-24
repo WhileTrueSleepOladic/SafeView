39cfa2cecd5fc67bd989c61c21d584f5e585aae5
Draymoor <iris250100@gmail.com>
2024-11-24 12:53:55 +0300

    Add files via upload

--- SafeView.cpp
+++ SafeView.cpp
@@ -1,77 +1,91 @@
 ﻿#include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
 #include <iostream>
+#include <fstream>
+#include <algorithm>
 
 using namespace std;
 using namespace cv;
 using namespace cv::dnn;
 
 const float CONFIDENCE_THRESHOLD = 0.4;
-const int INPUT_WIDTH = 500;
-const int INPUT_HEIGHT = 500;
+const int INPUT_WIDTH = 640;
+const int INPUT_HEIGHT = 320;
 
 // Пути к модели и конфигурации
-// 
-// const string MODEL_PATH = "D:/Apps/OpenCV/opencv/sources/samples/dnn/face_detector/linear_svm.xml";
-// const string MODEL_PATH = "D:/Apps/OpenCV/opencv/sources/samples/dnn/face_detector/new_model_1.caffemodel";
-// const string MODEL_PATH = "D:/Apps/OpenCV/opencv/sources/samples/dnn/face_detector/new_model_2.caffemodel";
-// const string MODEL_PATH = "D:/Apps/OpenCV/opencv/sources/samples/dnn/face_detector/bvlc_alexnet.caffemodel";
+const string MODEL_PATH = "C:/opencv/sources/samples/dnn/face_detector/res10_300x300_ssd_iter_140000.caffemodel";
+const string CONFIG_PATH = "C:/opencv/sources/samples/dnn/face_detector/deploy.prototxt";
 
-const string MODEL_PATH = "D:/Apps/OpenCV/opencv/sources/samples/dnn/face_detector/res10_300x300_ssd_iter_140000.caffemodel";
-const string CONFIG_PATH = "D:/Apps/OpenCV/opencv/sources/samples/dnn/face_detector/deploy.prototxt";
+ofstream file("C:/Projects/pythonProject4/logs.txt");
+
+pair<string, string> read_params() {
+    string line;
+    pair<string, string> ans;
+    ifstream in("C:/Projects/pythonProject4/param.txt"); // окрываем файл для чтения
+    if (!in) {
+        cout << "Ошибка открытия файла!" << endl;
+        return ans; // или обработайте ошибку по-другому
+    }
+    getline(in, line);
+    ans.first = line;
+    getline(in, line);
+    ans.second = line;
+    in.close(); // закрываем файл
+    return ans;
+}
 
 
-// const int VIDEO_PATH = 0; // if need to see cam
-const string VIDEO_PATH = "D:/Apps/Videos/Input/human_faces_1_input.mp4";
-const string OUTPUT_VIDEO_PATH = "D:/Apps/Videos/Output/human_faces_1_output.mp4";
 
 void detectAndBlurFaces(Mat& frame, Net& net);
 
 int main() {
+    // Получаем пути к файлам
+    pair<string, string> ways = read_params();
+    string input_path = ways.first;
+    string output_path = ways.second;
+
     // Загрузка модели DNN
     Net net = readNetFromCaffe(CONFIG_PATH, MODEL_PATH);
     if (net.empty()) {
-        cerr << "Ошибка: Не удалось загрузить модель DNN!" << endl;
+        cerr << "DNN Error!" << endl;
         return -1;
     }
 
-    // Запуск захвата видео (веб-камера)
-    VideoCapture capture(VIDEO_PATH);
+    // Запуск видео 
+    VideoCapture capture(input_path);
+    capture.open(input_path);
     if (!capture.isOpened()) {
-        cerr << "Ошибка: Не удалось открыть камеру!" << endl;
-        return -1;
+        cerr << "Camera Error!" << endl;
+        return -5;
     }
-
+    file << "Video Captured" << endl;
     // Получение информации о видео
     int frameWidth = static_cast<int>(capture.get(CAP_PROP_FRAME_WIDTH));
     int frameHeight = static_cast<int>(capture.get(CAP_PROP_FRAME_HEIGHT));
     int fps = static_cast<int>(capture.get(CAP_PROP_FPS));
 
     // Создание VideoWriter
-    VideoWriter writer(OUTPUT_VIDEO_PATH, VideoWriter::fourcc('H', '2', '6', '4'), fps, Size(frameWidth, frameHeight));
+    VideoWriter writer(output_path, VideoWriter::fourcc('m', 'p', '4', 'v'), fps, Size(frameWidth, frameHeight));
     if (!writer.isOpened()) {
-        cerr << "Ошибка: Не удалось открыть выходной видеофайл!" << endl;
+        cerr << "Writer Error!" << endl;
         return -1;
     }
-
+    file << "Video Writer had opened" << endl;
     Mat frame;
     while (capture.read(frame)) {
         if (frame.empty()) break;
 
         // Обнаружение лиц и блюр
         detectAndBlurFaces(frame, net);
-
+        file << "frame bloored" << endl;
         // Запись кадра в итоговый файл
         writer.write(frame);
-
-
-        // Отображение кадра
-        // imshow("Обнаружение лиц с размытием", frame);
-        
+        file << "frame writed" << endl;
         char c = (char)waitKey(10);
         if (c == 27 || c == 'q' || c == 'й' || c == 'Й') break; // Выход
     }
-
+    file << "process one" << endl;
+    file.close();
     return 0;
 }
 
@@ -104,11 +118,7 @@ void detectAndBlurFaces(Mat& frame, Net& net) {
             // Размытие области лица
             Rect faceRect(x1, y1, x2 - x1, y2 - y1);
             Mat faceROI = frame(faceRect); // ROI лица
-            GaussianBlur(faceROI, faceROI, Size(51, 51), 30); // Применение размытия
-            
-            putText(frame, format("Confidence: %.2f", confidence), Point(x1, y1 - 10),
-                FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 0, 0), 1);
-               
-        }   
+            GaussianBlur(faceROI, faceROI, Size(51, 51), 30); // Применение размытия     
+        }
     }
 }
